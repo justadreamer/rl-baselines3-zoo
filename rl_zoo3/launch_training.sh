5c3a6201d84b469db69c68bb609e0f56685ec0bb
Eugene Dorfman <eugene.dorfman@gmail.com>
2022-10-19 00:23:35 +0200

    lowered exploration (because we are using a trained model)

---
+++ rl_zoo3/launch_training.sh
@@ -0,0 +1,8 @@
+ train.py \
+        --device mps \
+        --wandb-entity justadreamer2007 \
+        --wandb-project-name dino-dqn \
+        --algo dqn \
+        --env DinoEnv-v0 \
+        --tensorboard-log tblogs \
+        --trained-agent logs/dqn/DinoEnv-v0_5/best_model.zip
