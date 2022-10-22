c044eef9b7d46b15cdac9f6a0a8dc7cea5337f6a
Eugene Dorfman <eugene.dorfman@gmail.com>
2022-10-22 21:38:03 +0200

    features_dim=1024 and mark_action:False

--- launch_training.sh
+++ launch_training.sh
@@ -4,5 +4,7 @@
         --wandb-project-name dino-dqn \
         --algo dqn \
         --env DinoEnv-v0 \
+        --env-kwargs mark_action:False \
         --tensorboard-log tblogs
+
 #        --trained-agent logs/dqn/DinoEnv-v0_7/best_model.zip
