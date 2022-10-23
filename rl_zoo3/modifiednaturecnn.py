3c237ab2deacd1f3adc604db320b54bbd6931b5c
Eugene Dorfman <eugene.dorfman@gmail.com>
2022-10-23 23:39:46 +0200

    a new set of hyperparams: buffer_size=100k num_stack=2

--- rl_zoo3/modifiednaturecnn.py
+++ rl_zoo3/modifiednaturecnn.py
@@ -24,32 +24,32 @@ class ModifiedNatureCNN(BaseFeaturesExtractor):
 
         n_input_channels = observation_space.shape[0]
 
-        # self.cnn = nn.Sequential( #4 x 150 x 75
-        #     nn.Conv2d(n_input_channels, 32, kernel_size=8, stride=4, padding=0),  #32 x 37 x 18
-        #     nn.ReLU(),
-        #     nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),  #64 x 15 x 8
-        #     nn.ReLU(),
-        #     nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0), #64 x 13 x 6
-        #     nn.ReLU(),
-        #     nn.Flatten(),
-        # )
-
-
-        self.cnn = nn.Sequential( #3 x 75 x 150
-            nn.Conv2d(n_input_channels, 16, kernel_size=5, stride=1, padding=0),
+        self.cnn = nn.Sequential( #4 x 150 x 75
+            nn.Conv2d(n_input_channels, 32, kernel_size=8, stride=4, padding=0),  #32 x 37 x 18
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2), #16 x 36 x 74
-            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=0),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),  #64 x 15 x 8
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2), # 32 x 17 x 36
-            nn.Conv2d(32, 64, kernel_size=2, stride=1, padding=0),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0), #64 x 13 x 6
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3), # 64 x 7 x 17
-            nn.Conv2d(64, 128, kernel_size=2, stride=1, padding=0),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3), # 128 x 2 x 7
-            nn.Flatten()
+            nn.Flatten(),
         )
+
+
+        # self.cnn = nn.Sequential( #3 x 75 x 150
+        #     nn.Conv2d(n_input_channels, 16, kernel_size=5, stride=1, padding=0),
+        #     nn.ReLU(),
+        #     nn.MaxPool2d(kernel_size=2), #16 x 36 x 74
+        #     nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=0),
+        #     nn.ReLU(),
+        #     nn.MaxPool2d(kernel_size=2), # 32 x 17 x 36
+        #     nn.Conv2d(32, 64, kernel_size=2, stride=1, padding=0),
+        #     nn.ReLU(),
+        #     nn.MaxPool2d(kernel_size=3), # 64 x 7 x 17
+        #     nn.Conv2d(64, 128, kernel_size=2, stride=1, padding=0),
+        #     nn.ReLU(),
+        #     nn.MaxPool2d(kernel_size=3), # 128 x 2 x 7
+        #     nn.Flatten()
+        # )
 
         # Compute shape by doing one forward pass
         with th.no_grad():
