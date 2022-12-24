df40359e21e42cbfad29a6723f0f4a6d0f31911e
Eugene Dorfman <eugene.dorfman@gmail.com>
2022-12-24 13:26:57 +0100

    let's try to train without acceleration

--- launch_training.sh
+++ launch_training.sh
@@ -5,7 +5,7 @@
         --wandb-project-name dino-dqn \
         --algo dqn \
         --env DinoEnv-v0 \
-        --env-kwargs mark_action:False \
+        --env-kwargs mark_action:False accelerate:False \
         --tensorboard-log tblogs
 
 #        --trained-agent logs/dqn/DinoEnv-v0_7/best_model.zip
