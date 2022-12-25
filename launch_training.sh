41b350704f9d0b541aea0e01595e0511377369c9
Eugene Dorfman <eugene.dorfman@gmail.com>
2022-12-25 17:35:45 +0100

    no tracking fixed

--- launch_training.sh
+++ launch_training.sh
@@ -1,11 +1,10 @@
  python train.py \
         --device mps \
-#        --track \
         --wandb-entity justadreamer2007 \
         --wandb-project-name dino-dqn \
         --algo dqn \
         --env DinoEnv-v0 \
         --env-kwargs mark_action:False accelerate:False \
         --tensorboard-log tblogs
-
+#        --track \
 #        --trained-agent logs/dqn/DinoEnv-v0_7/best_model.zip
