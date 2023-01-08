0801ba7de337ce60982556cf122e02ca184ce8ed
Eugene Dorfman <eugene.dorfman@gmail.com>
2023-01-08 11:46:54 +0100

    frequent evals to see if it is done correctly

--- launch_training.sh
+++ launch_training.sh
@@ -5,6 +5,7 @@
         --algo dqn \
         --env DinoEnv-v0 \
         --env-kwargs mark_action:False accelerate:False \
+        --eval-freq 5000 \
         --tensorboard-log tblogs \
         --track \
 #        --trained-agent logs/dqn/DinoEnv-v0_7/best_model.zip
