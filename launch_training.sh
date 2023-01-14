7ba4b403e57a0c5787c4b71c1220489e72fe05cc
Eugene Dorfman <eugene.dorfman@gmail.com>
2023-01-14 12:22:15 +0100

    added git pull to launch_training command

--- launch_training.sh
+++ launch_training.sh
@@ -1,11 +1,12 @@
- python train.py \
-        --device mps \
-        --wandb-entity justadreamer2007 \
-        --wandb-project-name dino-dqn \
-        --algo dqn \
-        --env DinoEnv-v0 \
-        --env-kwargs mark_action:False accelerate:False \
-        --eval-freq 5000 \
-        --tensorboard-log tblogs \
-        --track \
+git pull
+python train.py \
+      --device mps \
+      --wandb-entity justadreamer2007 \
+      --wandb-project-name dino-dqn \
+      --algo dqn \
+      --env DinoEnv-v0 \
+      --env-kwargs mark_action:False accelerate:False \
+      --eval-freq 5000 \
+      --tensorboard-log tblogs \
+      --track \
 #        --trained-agent logs/dqn/DinoEnv-v0_7/best_model.zip
