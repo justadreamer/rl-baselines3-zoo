66f57865104318deecece8b14276b1f611808b7b
Eugene Dorfman <eugene.dorfman@gmail.com>
2023-01-17 16:51:04 +0100

    let's train for 1MM steps at 40k buffer

--- launch_training.sh
+++ launch_training.sh
@@ -9,4 +9,4 @@ python train.py \
       --eval-freq 4000 \
       --tensorboard-log tblogs \
       --track \
-#      --trained-agent logs/dqn/DinoEnv-v0_35/best_model.zip
+#      --trained-agent logs/dqn/DinoEnv-v0_40/best_model.zip
